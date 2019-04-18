ce61f82a4cf2ba46519c8658ff6d552dde5605cc
yang <1113582818@qq.com>
2019-04-18 17:09:59 +0800

    tmd很气

--- client/login.go
+++ client/login.go
@@ -52,6 +52,9 @@ func login(username string, userpwd string) (err error) {
 		return
 	}
 
+	//我tmd很气
+	fmt.Printf("客户端发送数据长度=%d 内容=%s", pkgLen, string(data))
+	fmt.Printf("客户端发送数据长度=%d 内容=%s", pkgLen, string(data))
 	fmt.Printf("客户端发送数据长度=%d 内容=%s", pkgLen, string(data))
 
 	return nil
